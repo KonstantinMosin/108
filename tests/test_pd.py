5baa3cf6a3ccbd5f568ab45a0c1285e069828ee7
KonstantinMosin <msnknstntn@gmail.com>
2023-01-27 15:12:28 +0300

    Update tests

--- tests/test_pd.py
+++ tests/test_pd.py
@@ -1,12 +1,9 @@
-from proto import message_pb2
 from src.messageTools import parseDelimited
+from proto import message_pb2
 
 import unittest
 
 class ParseDelimitedTest(unittest.TestCase):
-    def setUp(self):
-        self.message = message_pb2.WrapperMessage()
-
-    def test_nulldata(self):
-        self.assertIsNone(parseDelimited(None))
-        self.assertIsNone(parseDelimited(""))
+    def test_null_data(self):
+        self.assertIsNone(parseDelimited(None, message_pb2.WrapperMessage()))
+        self.assertIsNone(parseDelimited("", message_pb2.WrapperMessage()))
