ad14a8c00f2687573644c3e757a0d424bda5a10b
KonstantinMosin <msnknstntn@gmail.com>
2023-01-27 14:53:55 +0300

    Update messageTools.py

--- src/messageTools.py
+++ src/messageTools.py
@@ -1,3 +1,36 @@
-def parseDelimited(data, size):
-    print(55)
-    return None
+from proto import message_pb2
+from google.protobuf.internal.decoder import _DecodeVarint32
+
+def parseDelimited(data):
+    if data is None or len(data) == 0:
+        return None
+    
+    
+    n = 0
+    while n < len(data):
+        length, pos = _DecodeVarint32(data, n)
+        n = pos
+
+        buffer = data[n:n + length]
+        n += length
+
+        message = message_pb2.WrapperMessage()
+        message.ParseFromString(buffer)
+
+    return message, length + pos
+
+class DelimitedMessagesStreamParser:
+    def __init__(self) -> None:
+        self.buffer = []
+
+    def parse(self, data):
+        self.buffer.append(data)
+
+        messages = []
+        while (len(self.buffer)):
+            message, bytesConsumed = parseDelimited(self.buffer, len(self.buffer))
+            if message:
+                messages.append(message)
+            self.buffer = self.buffer[bytesConsumed:]
+        
+        return messages
