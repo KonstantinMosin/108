eb4c5c5330c04f524e24dca7ca1a27598cb58210
KonstantinMosin <msnknstntn@gmail.com>
2023-01-31 17:54:11 +0300

    Update tests

--- tests/test_dmsp.py
+++ tests/test_dmsp.py
@@ -1,6 +1,8 @@
 from src.messageTools import DelimitedMessagesStreamParser
 from proto.message_pb2 import WrapperMessage
 
+from google.protobuf.internal.encoder import _VarintBytes
+
 import unittest
 
 class DelimitedMessagesStreamParserTest(unittest.TestCase):
@@ -36,5 +38,9 @@ class DelimitedMessagesStreamParserTest(unittest.TestCase):
     
     def test_corrupted_data(self):
         # TODO
+        message = WrapperMessage()
+        message.fast_response.current_date_time = "1"
+        message = _VarintBytes(message.ByteSize()) + message.SerializeToString() # \x05\n\x03\n\x011
+        
         pass
         
