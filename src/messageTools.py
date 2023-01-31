f19631c3588550b580f67b8176995a3d90426474
KonstantinMosin <msnknstntn@gmail.com>
2023-01-31 16:24:42 +0300

    Update messageTools.py

--- src/messageTools.py
+++ src/messageTools.py
@@ -1,36 +1,31 @@
+from typing import TypeVar, Any, Type
 from google.protobuf.internal.decoder import _DecodeVarint32
 
-def parseDelimited(data, typename):
-    def converter(type):
-        return typename(type)
+T = TypeVar('T')
 
+def parseDelimited(data: Any, message_type: Type[T]) -> Type[T]:
     if data is None or len(data) == 0:
         return None
+
+    length, pos = _DecodeVarint32(data, 0)
+    buffer = data[pos:pos + length]
     
+    message = message_type()
+    message.ParseFromString(buffer)
     
-    n = 0
-    while n < len(data):
-        length, pos = _DecodeVarint32(data, n)
-        n = pos
-
-        buffer = data[n:n + length]
-        n += length
-
-        message = converter(typename)
-        message.ParseFromString(buffer)
-
-    return message, length + pos
+    return message, pos + length
 
 class DelimitedMessagesStreamParser:
-    def __init__(self) -> None:
-        self.buffer = []
+    def __init__(self, type: Type[T]) -> None:
+        self.type = type
+        self.buffer = b''
 
     def parse(self, data):
-        self.buffer.append(data)
+        self.buffer += data
 
         messages = []
         while (len(self.buffer)):
-            message, bytesConsumed = parseDelimited(self.buffer, len(self.buffer))
+            message, bytesConsumed = parseDelimited(self.buffer, self.type)
             if message:
                 messages.append(message)
             self.buffer = self.buffer[bytesConsumed:]
