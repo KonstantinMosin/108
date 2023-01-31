fb33f67845fc0c5e6d11bf0fb190ccd0b0acc461
KonstantinMosin <msnknstntn@gmail.com>
2023-01-31 17:34:02 +0300

    Update tests

--- tests/test_dmsp.py
+++ tests/test_dmsp.py
@@ -6,4 +6,35 @@ import unittest
 class DelimitedMessagesStreamParserTest(unittest.TestCase):
     def test_null_data(self):
         parser = DelimitedMessagesStreamParser(WrapperMessage)
-        parser.parse(b'')
+        
+        messages = parser.parse(b'')
+        self.assertListEqual([], messages)
+        """
+        An empty list is created before parsing
+        """
+
+        messages = parser.parse("")
+        self.assertIsNone(messages)
+        """
+        The function takes bytes
+        """
+
+        messages = parser.parse(None)
+        self.assertIsNone(messages)
+        """
+        The function takes bytes
+        """
+
+    def test_wrong_data(self):
+        parser = DelimitedMessagesStreamParser(WrapperMessage)
+        
+        messages = parser.parse(b'\x04TEST')
+        self.assertListEqual([], messages)
+
+        messages = parser.parse(b'\x0A\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01')
+        self.assertListEqual([], messages)
+    
+    def test_corrupted_data(self):
+        # TODO
+        pass
+        
--- tests/test_pd.py
+++ tests/test_pd.py
@@ -1,9 +1,51 @@
-from src.messageTools import parseDelimited
 from proto.message_pb2 import WrapperMessage
+from src.messageTools import parseDelimited
+
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.internal.encoder import _VarintBytes
 
 import unittest
 
 class ParseDelimitedTest(unittest.TestCase):
     def test_null_data(self):
-        self.assertIsNone(parseDelimited(None, WrapperMessage))
-        self.assertIsNone(parseDelimited("", WrapperMessage))
+        message, bytesConsumed = parseDelimited(b'', WrapperMessage)
+        self.assertIsNone(message)
+
+        message, bytesConsumed = parseDelimited("", WrapperMessage)
+        self.assertIsNone(message)
+
+        message, bytesConsumed = parseDelimited(None, WrapperMessage)
+        self.assertIsNone(message)
+
+    def test_wrong_data(self):
+        message, bytesConsumed = parseDelimited(b'Hello World', WrapperMessage)
+        self.assertIsNone(message)
+        
+        message, bytesConsumed = parseDelimited(b'\x02\x04\x1A', WrapperMessage)
+        self.assertIsNone(message)
+
+    def test_bytes_consumed(self):
+        message = WrapperMessage()
+        message.request_for_fast_response.SetInParent()
+        message = _VarintBytes(message.ByteSize()) + message.SerializeToString()
+
+        length = len(message)
+
+        message, bytesConsumed = parseDelimited(message, WrapperMessage)
+        self.assertEqual(length, bytesConsumed)
+
+        message, bytesConsumed = parseDelimited(b'', WrapperMessage)
+        self.assertEqual(0, bytesConsumed)
+
+        buffer = b'Hello world'
+        message, bytesConsumed = parseDelimited(buffer, WrapperMessage)
+        self.assertEqual(0, bytesConsumed)
+        """
+        Hexadecimal "H" is 0x48 (ASCII).
+        The length of the 'ello world' message is 9 bytes,
+        so the function is waiting for the rest of the message.
+        """
+
+        buffer = b'\x02\x04\x1A'
+        message, bytesConsumed = parseDelimited(b'\x02\x04\x1A', WrapperMessage)
+        self.assertEqual(len(buffer), bytesConsumed)
