ca16cbb6fd7f6f0116b39042876e826c1795a48f
KonstantinMosin <msnknstntn@gmail.com>
2023-01-31 17:33:39 +0300

    Update messageTools.py

--- src/messageTools.py
+++ src/messageTools.py
@@ -5,25 +5,35 @@ T = TypeVar('T')
 
 def parseDelimited(data: Any, message_type: Type[T]) -> Type[T]:
     if data is None or len(data) == 0:
-        return None
-
+        return None, 0
+    
     length, pos = _DecodeVarint32(data, 0)
+
+    if (length > len(data[pos:])):
+        return None, 0
+
     buffer = data[pos:pos + length]
     
     message = message_type()
-    message.ParseFromString(buffer)
+    try:
+        message.ParseFromString(buffer)
+    except:
+        return None, pos + length
     
     return message, pos + length
 
 class DelimitedMessagesStreamParser:
     def __init__(self, type: Type[T]) -> None:
         self.type = type
-        self.buffer = b''
+        self.buffer: bytes = b''
 
-    def parse(self, data):
-        self.buffer += data
+    def parse(self, data: Any) -> list[Type[T]]:
+        try:
+            self.buffer += data
+        except:
+            return None
 
-        messages = []
+        messages: list[Type[T]] = []
         while (len(self.buffer)):
             message, bytesConsumed = parseDelimited(self.buffer, self.type)
             if message:
